f130551b37eaa2a98342d339219826700e89b8da
Theresa <Theresa.Robinson@CDSAzure.onmicrosoft.com>
2020-04-17 16:10:45 +0200

    This reverts commit a8c166ec for template.py, which was accidentally changed

--- template.py
+++ template.py
@@ -9,32 +9,9 @@ import re
 
 import advent_tools
 
-def move_dir(x, y, direction):
-    if direction == 'n':
-        y = y - 2
-    if direction == 's':
-        y = y + 2
-    if direction == 'nw':
-        x = x - 1
-        y = y - 1
-    if direction == 'ne':
-        x = x + 1
-        y = y - 1
-    if direction == 'sw':
-        x = x - 1
-        y = y + 1
-    if direction == 'se':
-        x = x + 1
-        y = y + 1
-    return x, y
 
 def run_part_1():
-    x = 0
-    y = 0
-    for line in advent_tools.read_whole_input().split(','):
-        x, y = move_dir(x, y, line)
-    print(x, y)
-
+    pass
 
 
 def run_part_2():
